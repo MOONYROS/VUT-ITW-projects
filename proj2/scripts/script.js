d825be582957d065c671283161cf24e5006480ac
Ondrej Lukasek <ondrej@lukasek.cz>
2023-04-22 17:44:11 +0200

    Code style fix

--- proj2/scripts/script.js
+++ proj2/scripts/script.js
@@ -15,33 +15,31 @@ document.querySelectorAll(".navLink").forEach(n => n.
 )
 
 // KOD PRO VETSI SKOK PRI PRESOUVANI NA POLOZKY MENU
-// get the height of the fixed menu
 const menuHeight = document.querySelector('.navContainer').offsetHeight + 40;
 
-// add event listeners to all the links in the menu
 document.querySelectorAll('.navContainer a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href');
-    const targetElement = document.querySelector(targetId);
-    const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
-    window.scrollTo({
-      top: targetPosition - menuHeight,
-      behavior: 'smooth'
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({
+            top: targetPosition - menuHeight,
+            behavior: 'smooth'
+        });
     });
-  });
 });
 
 // The last link should go also all the way to the top
 document.querySelectorAll('footer a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-      const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
-      window.scrollTo({
-        top: targetPosition - menuHeight,
-        behavior: 'smooth'
-      });
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({
+            top: targetPosition - menuHeight,
+            behavior: 'smooth'
+        });
     });
-  });
+});
