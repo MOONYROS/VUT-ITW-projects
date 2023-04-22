6eb1e9cb458a20a0ac8d95a58172a85f91312967
Ondrej Lukasek <ondrej@lukasek.cz>
2023-04-22 19:38:31 +0200

    viewport activity added for phones

--- proj2/scripts/script.js
+++ proj2/scripts/script.js
@@ -43,3 +43,40 @@ document.querySelectorAll('footer a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// POKUSY
+
+// Set the options for the observer
+const options = {
+    root: null, // Use the document viewport as the container
+    rootMargin: '0px', // No margin
+    threshold: 0.99, // The element is considered in viewport when 80% of it is visible
+};
+
+// Create a new Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+    // Loop through the entries
+    entries.forEach((entry) => {
+        // If the element is in viewport, add the 'active' class
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        } 
+        else {
+            // Otherwise, remove the 'active' class
+            entry.target.classList.remove('active');
+        }
+    });
+}, options);
+  
+  
+// Watch for changes in the visibility of elements with the class
+observer.observe(document.querySelector('#headerDiv'));
+observer.observe(document.querySelector('#GeneralInfo'));
+observer.observe(document.querySelector('#About'));
+observer.observe(document.querySelector('#School2'));
+observer.observe(document.querySelector('#School1'));
+observer.observe(document.querySelector('#Hobbies'));
+observer.observe(document.querySelector('#Work'));
+observer.observe(document.querySelector('#Contact'));
+observer.observe(document.querySelector('footer img'));
+observer.observe(document.querySelector('footer'));
